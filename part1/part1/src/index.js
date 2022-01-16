3447bfbe409899d0a6549afee96aa17c373bb07d
datgerhardt <datsomorg@gmail.com>
2022-01-16 20:59:23 +0000

    stateful component

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -1,22 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 
-let counter = 1
-const refresh = () => {
-    ReactDOM.render(
-      <App counter={counter}/>,
+ReactDOM.render(
+      <App />,
     document.getElementById('root')
   );
-
-}
-
-// refresh()
-// counter++
-// refresh()
-// counter++
-// refresh()
-
-setInterval(() => {
-  refresh()
-  counter++
-}, 1000)
