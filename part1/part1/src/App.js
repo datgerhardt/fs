42423ed71110972e41b04ab336d4ba85b25a8039
datgerhardt <datsomorg@gmail.com>
2022-01-16 19:56:24 +0000

    Destructuring

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,13 +1,15 @@
 import { Component } from "react";
 
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+const Hello = ({name, age}) => {
+  // const name = props.name
+  // const age = props.age
+  // const {name, age} = props
+
+  const bornYear = () =>  new Date().getFullYear() - age
+  
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
