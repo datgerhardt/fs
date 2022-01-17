7ec4757e1ebaf95863244732b97d234ccecb71e5
datgerhardt <datsomorg@gmail.com>
2022-01-17 10:38:08 +0000

    Part 1d

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,37 +1,22 @@
 import React, {useState} from 'react';
 
-const Display = ({counter}) => {
-  return(
-    <div>{counter}</div>
-  )
-}
-const Button = ({onClick, text}) => {
-  return(
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  
 
-    setTimeout(
-      () => setCounter(counter +1),
-      1000
-    )
-
-    const  increaseByOne = () => setCounter(counter +1)
-    const  decreaseByOne = () => setCounter(counter +1)
-    const  setToZero = () => setCounter(0)
-
-    return (
+  return (
     <div>
-      <Display counter={counter}/>
-      <Button text={'Plus'} onClick={increaseByOne} />
-      <Button text={'Minus'} onClick={decreaseByOne} />
-      <Button text={'Zero'} onClick={setToZero} />
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
-  )}
+  )
+}
   
 export default App;
