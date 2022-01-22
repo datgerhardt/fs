363a9029ef5969acaecb0db1f2cf99546bbe9807
datgerhardt <datsomorg@gmail.com>
2022-01-22 13:31:07 +0000

    Done with exercise 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Display = ({selected, anecdotes}) => {
+
   if (anecdotes.length === selected +1){
     return(
       <p>End of array </p>
@@ -22,24 +23,32 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
+  
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
 
-  const handleClick = () => {
+
+
+  const handleNextClick = () => {
     if (anecdotes.length <= selected+1){
       setSelected(selected)
     }else{
       setSelected(selected +1)
     }
   }
+  const handleVoteClick= () => {
+    let copy = [...points]
+    copy[selected] += 1
+    setPoints([...copy])
 
+  }
   return (
     <div>
       <Display anecdotes={anecdotes} selected={selected}/>
-      {/* <p>{anecdotes[selected]}</p> */}
-      <button onClick={handleClick}> next anecdotes</button>
+      <button onClick={handleVoteClick}>vote</button>
+      <button onClick={handleNextClick}> next anecdotes</button>
     </div>
   )
 }
-
+ 
 export default App
