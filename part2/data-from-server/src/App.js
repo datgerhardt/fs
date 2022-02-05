4de1393139dd59b00822be17c9c17e3a663ee960
datgerhardt <datsomorg@gmail.com>
2022-02-05 12:51:12 +0000

    Effect hooks part

--- part2/data-from-server/src/App.js
+++ part2/data-from-server/src/App.js
@@ -1,12 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
- const App = ({notes}) => {
+ const App = () => {
+  const [notes, setNotes] = useState([]) 
+  // const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  
+  const hook = () => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then(
+      res => {  
+        console.log('promise fulfilled');
+        setNotes(res.data)            
+      })
+  }
+  
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+
+  const noteToShow = showAll ? notes : notes.filter(note => note.important === true)
+  
   return (
-    <div >
+    <div>
+      <h1>Notes</h1>
+      <div> 
+        <button onClick={()=> setShowAll(!showAll)}>
+          show { showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => <li key={note.id}> {note.content} </li>)}
+        {noteToShow.map(note =>  
+          <Note key={note.id} note={note}/>
+        )}
       </ul>
     </div>
-  );
+  )
 }
 
 export default App;
--- part2/data-from-server/src/index.js
+++ part2/data-from-server/src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from 'axios';
 
 
-let url = 'http://localhost:3001/'
-axios.get(url+'notes').then(
-  res => {  
-    const notes = res.data 
-    
-    ReactDOM.render(
-      <App  notes={notes}/>,
-    document.getElementById('root')
-    );
-
-  })
-// promise.then(res => console.log(res))
-
-// const promise2 = axios.get(url+'foobar')
-// console.log(promise2)
-
+ReactDOM.render(
+  <App />,
+document.getElementById('root')
+);
