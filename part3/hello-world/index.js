062806240b509a6c4f713ef1377bea257cba6452
datgerhardt <datsomorg@gmail.com>
2022-02-07 15:11:10 +0000

    simple web server

--- part3/hello-world/index.js
+++ part3/hello-world/index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Hello, world')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
