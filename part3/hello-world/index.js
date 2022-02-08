da02c127ed0e2d61f5242233dc23c28dfc59f8e9
datgerhardt <datsomorg@gmail.com>
2022-02-08 01:44:42 +0000

    middleware

--- part3/hello-world/index.js
+++ part3/hello-world/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let notes = [
     {
@@ -47,6 +58,34 @@ app.delete('/api/notes/:id', (req, res) => {
     
     res.status(204).end()   
 })
+
+app.post('/api/notes', (req, res) => {
+    const body= req.body
+    if (!body.content){
+       return res.status(400).json({error:"content is missing"}) 
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+    
+    notes.concat(note)
+    res.json(note)
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)): 0
+    return maxId + 1
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
